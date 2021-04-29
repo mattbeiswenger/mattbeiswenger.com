2822e92075fc3281163ddd5f3614bc480f95ee62
Matt Beiswenger <mattbeis@yahoo.com>
2021-04-28 21:41:58 -0500

    Fix styling

--- components/inline-link.js
+++ components/inline-link.js
@@ -4,11 +4,12 @@ function InlineLink({ url, children, newTab }) {
   const target = newTab && '_blank'
   const rel = newTab && 'noopener noreferrer'
   return (
-    <Link
-      className="inline-block transition duration-200 ease-in-out bg-light-grey hover:bg-light-grey-hover py-0.5 px-1 rounded-lg whitespace-no-wrap"
-      href={url}
-    >
-      <a target={target} rel={rel}>
+    <Link href={url}>
+      <a
+        className="inline-block transition duration-200 ease-in-out bg-light-grey hover:bg-light-grey-hover py-0.5 px-1 text-sm rounded-lg whitespace-no-wrap"
+        target={target}
+        rel={rel}
+      >
         {children}
       </a>
     </Link>
