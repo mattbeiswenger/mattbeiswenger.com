98343a312d8fa8c63b342ce651c4435801e2f87d
Matt Beiswenger <mattbeis@yahoo.com>
2021-05-19 21:28:06 -0500

    Add published date and reading time

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,8 +2,9 @@ import { getFileBySlug, getFiles } from '@/lib/mdx'
 import { MDXRemote } from 'next-mdx-remote'
 import Head from 'next/head'
 import Container from '@/components/container'
+import Date from '@/components/date'
 
-export default function Post({ source, metadata }) {
+export default function Post({ source, metadata, readingTime }) {
   return (
     <Container>
       <Head>
@@ -11,6 +12,11 @@ export default function Post({ source, metadata }) {
       </Head>
       <article className="leading-7 md:leading-8 text-gray-800 max-w-prose dark:text-gray-300">
         <h1 className="text-2xl font-semibold md:text-4xl">{metadata.title}</h1>
+        <div className="mt-2 flex gap-2 text-gray-400">
+          <Date dateString={metadata.published} />
+          <div>/</div>
+          <div>{readingTime}</div>
+        </div>
         <MDXRemote {...source} />
       </article>
     </Container>
