44a90b972e9899afdc69810e5c1d0ff19af8277f
Matt Beiswenger <mattbeis@yahoo.com>
2021-07-02 21:47:04 -0500

    Add google site verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
           />
+          <meta name="google-site-verification" content="cIF7EczaClpfmh9I1iDr-MS9SJDiNnK47yv0vsDy-l4" />
           <link rel="icon" href="/favicon-16x16.png" />
           <link rel="icon" href="/favicon-32x32.png" />
           <link rel="icon" href="/favicon-96x96.png" />
