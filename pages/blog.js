db8a2171c828d26393a933c6e8b2eadaf0e0cea1
Matt Beiswenger <mattbeis@yahoo.com>
2021-07-07 21:11:59 -0500

    Add spacing between blog posts

--- pages/blog.js
+++ pages/blog.js
@@ -5,20 +5,22 @@ import Link from 'next/link'
 export default function Home({ posts }) {
   return (
     <Container title="Blog - Matt Beiswenger">
-      {posts.map((post) => {
-        return (
-          <Link key={post.slug} href={`blog/${post.slug}`}>
-            <div className="cursor-pointer grid gap-1 w-full">
-              <a className="dark:text-white text-lg md:text-2xl font-medium">
-                {post.title}
-              </a>
-              <div className="dark:text-gray-400 leading-relaxed text-sm md:text-md text-gray-600">
-                {post.description}
+      <div className="grid gap-10">
+        {posts.map((post) => {
+          return (
+            <Link key={post.slug} href={`blog/${post.slug}`}>
+              <div className="cursor-pointer grid gap-1 w-full">
+                <a className="dark:text-white text-lg md:text-2xl font-medium">
+                  {post.title}
+                </a>
+                <div className="dark:text-gray-400 leading-relaxed text-sm md:text-md text-gray-600">
+                  {post.description}
+                </div>
               </div>
-            </div>
-          </Link>
-        )
-      })}
+            </Link>
+          )
+        })}
+      </div>
     </Container>
   )
 }
