75c22131e639d3497f0099095f7f249ad44ae04a
Matt Beiswenger <mattbeis@yahoo.com>
2021-12-17 23:47:56 -0600

    fix image styles

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -6,8 +6,8 @@ export default function ContentCard({ href, image, published, time, title }) {
   return (
     <Link href={href}>
       <a className="p-4 transition bg-white border-2 dark:border-transparent dark:bg-gray-800 rounded-xl ring-opacity-80 hover:ring-2 ring-offset-4 dark:ring-offset-gray-900 ring-red-400">
-        <div className="block rounded-xl responsive-image">
-          <Image src={image} layout="fill" />
+        <div className="rounded-xl responsive-image">
+          <Image src={image} layout="fill" objectFit="cover" />
         </div>
         <div className="flex gap-2 mt-3 text-sm text-gray-600 dark:text-gray-400">
           <Date dateString={published} />
