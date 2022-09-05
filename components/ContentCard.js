6a3fb7091f00b8475d6872c6bab0127f125f69d5
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-05 14:50:47 -0500

    feat: add books to site homepage

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -4,8 +4,8 @@ import Link from 'next/link'
 export default function ContentCard({ href, published, time, title }) {
   return (
     <Link href={href}>
-      <a className="p-4 transition bg-white border-2 dark:border-transparent dark:bg-neutral-800 rounded-xl">
-        <div className="sm:text-xl text-md font-medium text-neutral-800 dark:text-neutral-100">
+      <a className="p-4 transition bg-white border dark:border-transparent dark:bg-neutral-800 rounded-xl">
+        <div className="font-semibold sm:text-xl text-md text-neutral-800 dark:text-neutral-100">
           {title}
         </div>
         <div className="flex gap-2 mt-3 text-sm text-neutral-600 dark:text-neutral-400">
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Container from '@/components/Container'
 import ContentCard from '@/components/ContentCard'
 import { getAllPostsMetadata } from '@/lib/mdx'
+import { getAllBooks } from '@/lib/oku'
+import { BookOpenIcon, CheckCircleIcon } from '@heroicons/react/outline'
+import BookCard from '@/components/BookCard'
 
-export default function Home({ articles }) {
+export default function Home({ articles, books }) {
   return (
     <>
       <Container title="Matt Beiswenger">
-        <div className="max-w-2xl text-neutral-900 sm:text-2xl text-lg tracking-wide sm:mt-7 mt-5 dark:text-neutral-200">
+        <div className="max-w-2xl mt-5 text-lg tracking-wide text-neutral-900 sm:text-2xl sm:mt-7 dark:text-neutral-200">
           Hi, I’m Matt Beiswenger
         </div>
-        <div className="max-w-2xl text-neutral-900 sm:text-2xl text-lg tracking-wide sm:mt-7 mt-5 dark:text-neutral-200">
+        <div className="max-w-2xl mt-5 text-lg tracking-wide text-neutral-900 sm:text-2xl sm:mt-7 dark:text-neutral-200">
           I&apos;m a design-minded software developer currently building and
           breaking things at{' '}
           <a
@@ -21,7 +24,7 @@ export default function Home({ articles }) {
             TopBloc
           </a>
         </div>
-        <div className="flex gap-5 pt-5 font-medium text-neutral-600 text-md dark:text-neutral-400 tracking-wide">
+        <div className="flex gap-5 pt-5 font-medium tracking-wide text-neutral-600 text-md dark:text-neutral-400">
           <a
             href="https://github.com/mattbeiswenger"
             rel="noreferrer"
@@ -44,9 +47,52 @@ export default function Home({ articles }) {
             LinkedIn
           </a>
         </div>
-        <section className="sm:mt-20 mt-10">
+        <section>
+          <div className="mt-10 mb-5 text-xl font-medium sm:text-3xl text-neutral-800 dark:text-neutral-100">
+            Activity
+          </div>
+          <div className="grid grid-cols-1 gap-10 sm:gap-40 sm:grid-cols-2">
+            <div>
+              <span className="flex items-center gap-1">
+                <BookOpenIcon className="w-4 h-4 text-indigo-400" />
+                <span className="text-sm font-semibold text-indigo-500 dark:text-indigo-300 sm:text-md">
+                  I'm currently reading
+                </span>
+              </span>
+              <div className="flex flex-col gap-3 mt-4">
+                {books.READING.map((book) => (
+                  <BookCard
+                    key={book.guid}
+                    link={book.link}
+                    title={book.title}
+                    author={book.creator}
+                  />
+                ))}
+              </div>
+            </div>
+            <div>
+              <span className="flex items-center gap-1">
+                <CheckCircleIcon className="w-4 h-4 text-emerald-500" />
+                <span className="text-sm font-semibold sm:text-md dark:text-emerald-300 text-emerald-600">
+                  I've recently finished
+                </span>
+              </span>
+              <div className="flex flex-col gap-3 mt-4">
+                {books.READ.map((book) => (
+                  <BookCard
+                    link={book.link}
+                    key={book.guid}
+                    title={book.title}
+                    author={book.creator}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="mt-10 sm:mt-20">
           <div className="flex flex-col gap-2">
-            <div className="sm:text-3xl text-xl font-medium text-neutral-800 dark:text-neutral-100">
+            <div className="text-xl font-medium sm:text-3xl text-neutral-800 dark:text-neutral-100">
               Recent Articles
             </div>
           </div>
@@ -71,9 +117,12 @@ export default function Home({ articles }) {
 
 export async function getStaticProps() {
   const articles = await getAllPostsMetadata('blog')
+  const books = await getAllBooks()
+
   return {
     props: {
       articles,
+      books,
     },
   }
 }
