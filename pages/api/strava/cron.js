5d1a114eefcaf0a768d77ceea1c6a365cd58cfb4
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-06 22:00:40 -0500

    feat: programmatically update strava environment variables

--- pages/api/strava/cron.js
+++ pages/api/strava/cron.js
@@ -1,6 +1,26 @@
 import { verifySignature } from '@upstash/qstash/nextjs'
 
+/**
+ * This lambda function is used to refresh the strava access token and update
+ * both the refresh and access token environment variables in vercel.
+ * This function gets triggered by qstash every 3 hours.
+ */
+
+async function updateEnvironmentVariable(id, value) {
+  await fetch(
+    `https://api.vercel.com/v9/projects/mattbeiswenger-com/env/${id}`,
+    {
+      method: 'PATCH',
+      body: { value },
+      headers: { Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` },
+    }
+  )
+}
+
 async function handler(req, res) {
+  const STRAVA_ACCESS_TOKEN_ID = 'n95wSDTs6fiVxtaK'
+  const STRAVA_REFRESH_TOKEN_ID = 'vADUJDMkV8VI2ufN'
+
   if (req.method === 'POST') {
     try {
       const params = new URLSearchParams({
@@ -9,14 +29,18 @@ async function handler(req, res) {
         grant_type: 'refresh_token',
         refresh_token: process.env.STRAVA_REFRESH_TOKEN,
       })
+
       const response = await fetch(
         `https://www.strava.com/api/v3/oauth/token?${params.toString()}`,
-        {
-          method: 'POST',
-        }
+        { method: 'POST' }
       )
-      const data = await response.json()
-      console.log(data)
+
+      const { access_token, refresh_token } = await response.json()
+      await Promise.all([
+        updateEnvironmentVariable(STRAVA_ACCESS_TOKEN_ID, access_token),
+        updateEnvironmentVariable(STRAVA_REFRESH_TOKEN_ID, refresh_token),
+      ])
+
       res.status(200).json({ success: true })
     } catch (err) {
       res.status(500).json({ statusCode: 500, message: err.message })
