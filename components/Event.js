d46f4f5c348a7c21ecd1ced08b61f65a8594ff6d
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-11 18:25:28 -0500

    feat: change text color of activity timestamp

--- components/Event.js
+++ components/Event.js
@@ -7,7 +7,7 @@ function Event({ children, title, startTime, node }) {
       <div className="flex flex-col w-full gap-3">
         <div className="flex items-center justify-between gap-2">
           {title('text-sm text-neutral-200 flex items-center gap-1')}
-          <span className="text-sm text-neutral-500">
+          <span className="text-sm text-neutral-400">
             {formatDistance(new Date(startTime), new Date())} ago
           </span>
         </div>
