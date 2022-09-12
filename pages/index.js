067cf4e4eca43c294a48aee89c58bc03ff93693c
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-11 22:48:58 -0500

    feat: update topbloc link

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import Container from '../components/Container'
-import ContentCard from '../components/ContentCard'
 import { getAllPostsMetadata } from '../lib/mdx'
 import { getAllBooks } from '../lib/oku'
-import { BookOpenIcon, CheckCircleIcon } from '@heroicons/react/24/outline'
-import BookCard from '../components/BookCard'
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
 import SmallContainer from '../components/SmallContainer'
 import { getStravaActivities } from '../lib/strava'
 import { getCommits } from '../lib/github'
-import { isAfter, parseISO, startOfDay, startOfToday, sub } from 'date-fns'
-import EventStream from '../components/EventStream'
-import Image from 'next/image'
+import { isAfter, startOfToday, sub } from 'date-fns'
 import EventFactory from '../components/EventFactory'
 import Link from 'next/link'
 
@@ -18,29 +14,30 @@ export default function Home({ articles, books, events }) {
     <>
       <Container title="Matt Beiswenger">
         <SmallContainer>
-          <div className="text-lg text-neutral-900 sm:mt-7 dark:text-neutral-200">
-            Matt Beiswenger
-          </div>
-          <div className="text-sm font-medium text-neutral-400">
-            Software Developer
-          </div>
-          <div className="mt-5 leading-7 text-neutral-900 sm:mt-7 dark:text-neutral-200">
-            I&apos;m a design-focused software developer currently building
-            products at{' '}
-            <a
-              className="font-medium text-neutral-400"
-              href="https://topbloc.com"
-              rel="noreferrer"
-              target="_blank"
-            >
-              TopBloc
-            </a>
-            <div>
-              Type{' '}
-              <span className="px-1 py-0.5 text-sm rounded-md bg-neutral-800 border border-neutral-700">
-                ⌘K
-              </span>{' '}
-              to explore this site
+          <div className="text-neutral-400">
+            <div className="text-lg mt-7 text-neutral-200">Matt Beiswenger</div>
+            <div className="text-sm font-medium text-neutral-400">
+              Software Developer
+            </div>
+            <div className="leading-7 mt-7 text-neutral-300">
+              I&apos;m a design-focused software developer currently building
+              products at{' '}
+              <a
+                className="flex items-center gap-1 text-neutral-200"
+                href="https://topbloc.com"
+                rel="noreferrer"
+                target="_blank"
+              >
+                TopBloc
+                <ArrowUpRightIcon className="w-3 h-3 stroke-neutral-300" />
+              </a>
+              <div className="mt-5">
+                Type{' '}
+                <span className="px-1 py-0.5 text-sm rounded-md bg-neutral-800 border border-neutral-700">
+                  ⌘K
+                </span>{' '}
+                to explore this site
+              </div>
             </div>
           </div>
           {/* <div className="flex gap-5 pt-5 font-medium tracking-wide text-neutral-600 text-md dark:text-neutral-400">
@@ -67,7 +64,7 @@ export default function Home({ articles, books, events }) {
             </a>
           </div> */}
           <section>
-            <div className="mt-10 mb-5 text-neutral-800 dark:text-neutral-200">
+            <div className="mt-8 mb-5 text-neutral-800 dark:text-neutral-300">
               Here's what I've been up to recently
             </div>
             <div className="flex flex-col justify-center gap-10 mx-auto mt-10 align-middle fade-bottom justify-items-center">
