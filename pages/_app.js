98981eb4f9c1345622ee026cdb8609d8fc5bfd49
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-17 13:26:56 -0500

    feat: create command palette

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import '../styles/global.css'
 import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '../components/MdxComponents'
+import CommandPalette from '../components/CommandPalette'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <MDXProvider components={MDXComponents}>
+        <CommandPalette />
         <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
