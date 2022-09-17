6332dc377725692abf9c7fdfe9caad4e35c852ff
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-17 11:11:41 -0500

    feat: make /articles page mobile friendly

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -4,9 +4,14 @@ import Link from 'next/link'
 export default function ContentCard({ href, published, title }) {
   return (
     <Link href={href}>
-      <a className="flex justify-between">
-        <div className="font-medium text-neutral-300">{title}</div>
-        <Date className="text-sm text-neutral-400" dateString={published} />
+      <a className="flex items-center justify-between gap-2">
+        <div className="text-sm font-medium truncate text-neutral-300 sm:text-base">
+          {title}
+        </div>
+        <Date
+          className="text-xs sm:text-sm text-neutral-400 whitespace-nowrap"
+          dateString={published}
+        />
       </a>
     </Link>
   )
