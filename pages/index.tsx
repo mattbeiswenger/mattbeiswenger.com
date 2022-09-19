7ba932a1139767adceac564636f44933ac340bc3
Matt Beiswenger <mattbeis@yahoo.com>
2022-09-18 23:26:20 -0500

    feat: allow pressing ⌘ K button to open command palette and make command palette more mobile friendly

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,15 @@ import Link from 'next/link'
 import Header from '../components/Header'
 import EventStream from '../components/EventStream'
 import { Event } from '../types'
+import { useCommandPaletteContext } from '../contexts/command-palette'
 
 type HomeProps = {
   events: Event[]
 }
 
 export default function Home({ events }: HomeProps) {
+  const [open, setOpen] = useCommandPaletteContext()
+
   return (
     <>
       <Container title="Matt Beiswenger">
@@ -23,7 +26,7 @@ export default function Home({ events }: HomeProps) {
           I&apos;m a design-focused software developer currently building
           products at{' '}
           <a
-            className="flex items-center gap-1 text-neutral-400"
+            className="inline-flex items-center gap-1 text-neutral-400"
             href="https://topbloc.com"
             rel="noreferrer"
             target="_blank"
@@ -32,10 +35,13 @@ export default function Home({ events }: HomeProps) {
             <ArrowUpRightIcon className="w-3 h-3 stroke-neutral-300" />
           </a>
           <div className="mt-5">
-            Type{' '}
-            <span className="px-1 py-0.5 text-sm rounded-md bg-neutral-800 border border-neutral-700">
+            Type or press{' '}
+            <button
+              onClick={() => setOpen(true)}
+              className="ring-0 px-1 py-0.5 text-sm rounded-md bg-neutral-800 border border-neutral-700"
+            >
               ⌘K
-            </span>{' '}
+            </button>{' '}
             to explore this site
           </div>
         </div>
--- pages/photos.tsx
+++ pages/photos.tsx
@@ -4,23 +4,32 @@ import path from 'path'
 import Image from 'next/image'
 import { ArrowLongLeftIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { useCommandPaletteContext } from '../contexts/command-palette'
 
 type PhotosProps = {
   photos: string[]
 }
 
 export default function Photos({ photos }: PhotosProps) {
+  const [open, setOpen] = useCommandPaletteContext()
+
   return (
     <Container
       title="Photos — Matt Beiswenger"
       className="!m-5 !max-w-none !p-0"
     >
       <Link href="/">
-        <a className="fixed z-10 flex items-center gap-2 px-4 py-2 text-sm rounded-lg group sm:top-14 sm:left-14 left-10 top-10 backdrop-blur-md bg-opacity-40 bg-neutral-700">
+        <a className="fixed z-10 flex items-center gap-2 px-3 py-1.5 text-sm rounded-lg group sm:top-14 sm:left-14 left-10 top-10 backdrop-blur-md bg-opacity-40 bg-neutral-700">
           <ArrowLongLeftIcon className="w-4 h-4 transition-transform group-hover:-translate-x-1" />
           Home
         </a>
       </Link>
+      <button
+        onClick={() => setOpen(true)}
+        className="fixed z-10 sm:top-14 sm:right-14 right-10 top-10 px-3 py-1.5 text-sm rounded-md ring-0 focus:ring-0 bg-neutral-800 backdrop-blur-md bg-opacity-40 "
+      >
+        ⌘K
+      </button>
       <div className="image-grid">
         {photos.map((photo) => {
           return (
