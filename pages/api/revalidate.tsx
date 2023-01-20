5175f2784e7784bad75b879b35000dafb71908c0
Matt Beiswenger <mattbeis@yahoo.com>
2023-01-19 20:12:52 -0600

    feat: add revalidation route

---
+++ pages/api/revalidate.tsx
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * Revalidate home page and activities page
+ * https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
+ */
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.body.secret !== process.env.REVALIDATION_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    await res.revalidate('/')
+    await res.revalidate('/activities')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).send('Error revalidating')
+  }
+}
+
+export default handler
