8f3e99e739f41e6fa8f63a3c78bf508d03fefa00
Matt Beiswenger <mattbeis@yahoo.com>
2023-01-21 12:05:48 -0600

    chore: add logs for troubleshooting parsing of webhook request

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -7,18 +7,22 @@ import { verifySignature } from '@upstash/qstash/nextjs'
  */
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!('secret' in req.body)) {
-    return res.status(401).json({ message: 'No token provided' })
-  }
-  if (req.body.secret !== process.env.REVALIDATION_TOKEN) {
-    return res.status(403).json({ message: 'Invalid token' })
-  }
+  console.log(req.body)
+  console.log(req.body.secret)
+
+  const body = await readBody(req)
+  console.log(body)
+
+  // if (!('secret' in req.body)) {
+  //   return res.status(401).json({ message: 'No token provided' })
+  // }
+  // if (req.body.secret !== process.env.REVALIDATION_TOKEN) {
+  //   return res.status(403).json({ message: 'Invalid token' })
+  // }
 
   try {
-    console.log('[Next.js] Revalidating /')
-    await res.revalidate('/')
-    console.log('[Next.js] Revalidating /activities')
-    await res.revalidate('/activities')
+    console.log('Revalidating...')
+    await Promise.all([res.revalidate('/'), res.revalidate('/activities')])
     return res.status(200).send('Success!')
   } catch (err) {
     return res.status(500).send('Error revalidating')
@@ -31,4 +35,12 @@ export const config = {
   api: {
     bodyParser: false,
   },
+}
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
 }
