c5e004086fd9b558eaf423c3ad720865c56693b4
Matt Beiswenger <mattbeis@yahoo.com>
2023-05-01 12:02:11 -0500

    chore: add analytics component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { createContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import CommandPalette from '../components/CommandPalette'
 import { CommandPaletteProvider } from '../contexts/command-palette'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -30,6 +31,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider defaultTheme="system" attribute="class">
       <CommandPaletteProvider>
         <CommandPalette />
+        <Analytics />
         <Component {...pageProps} />
       </CommandPaletteProvider>
     </ThemeProvider>
