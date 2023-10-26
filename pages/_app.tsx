75cfa3e22e21381918a327462f5d062208307b6b
Matt Beiswenger <mattbeis@yahoo.com>
2023-10-25 20:18:18 -0500

    remove sentry and other unused packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,12 @@
 import '../styles/global.css'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
-import * as Fathom from 'fathom-client'
-import { createContext, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { createContext, useState } from 'react'
 import CommandPalette from '../components/CommandPalette'
 import { CommandPaletteProvider } from '../contexts/command-palette'
 import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    Fathom.load('HMLMBPNT', {
-      includedDomains: ['mattbeiswenger.com'],
-    })
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
   const [open, setOpen] = useState(false)
   createContext([open, setOpen])
 
--- sentry.server.config.js
+++
@@ -1,6 +0,0 @@
-import * as Sentry from '@sentry/nextjs'
-
-Sentry.init({
-  dsn: 'https://c993dd7109e14d4fbf0ab33a7b444628@o346010.ingest.sentry.io/6761543',
-  tracesSampleRate: 1.0,
-})
